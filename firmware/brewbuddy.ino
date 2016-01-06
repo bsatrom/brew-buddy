676a7008f1dc088bd681f8abb97ab12e3f1aab08
Brandon Satrom <bsatrom@gmail.com>
2016-01-05 18:45:24 -0600

    added backend posting and counter on TFT

--- firmware/brewbuddy.ino
+++ firmware/brewbuddy.ino
@@ -1,27 +1,36 @@
+#include "math.h"
+
 // Thermocouple Includes
-//#include "SparkFunMAX31855k.h"
+#include "SparkFunMAX31855k.h"
 
 // TFT Includes
 #include "Adafruit_ILI9341.h"
 
 // Thermocouple Variables
-const uint8_t CHIP_SELECT_PIN = 10;
-// SCK & MISO are defined by Arduino
-const uint8_t VCC = 5;
-const uint8_t GND = 6;
+const uint8_t CHIP_SELECT_PIN = D3;
+// SCK, MISO & MOSI are defined by Spark on A3-A5
+const uint8_t VCC = D1;
+const uint8_t GND = D2;
 // Instantiate an instance of the SparkFunMAX31855k class
-//SparkFunMAX31855k probe(CHIP_SELECT_PIN, VCC, GND);
+SparkFunMAX31855k probe(CHIP_SELECT_PIN, VCC, GND);
 
-// TFT Variables
-#define _sclk 13
-#define _miso 12
-#define _mosi 11
-#define _cs 7
-#define _dc 9
-#define _rst 8
 // Use hardware SPI
 // cs = A2, dc = A1, rst = A0
 Adafruit_ILI9341 tft = Adafruit_ILI9341(A2, A1, A0);
+
+// Timing Variables
+unsigned long previousTempMillis = 0;
+unsigned long tempInterval = 5000;
+unsigned long startTime = 0;
+unsigned long elapsedTime;
+float previousTemp = 0;
+
+//Text Size Variables
+const uint8_t headingTextSize = 4;
+const uint8_t subheadTextSize = 2;
+const uint8_t tempTextSize = 7;
+const uint8_t elapsedTimeSize = 3;
+const uint8_t pixelMultiplier = 7; //Used to clear text portions of the screen
 
 void setup() {
   Serial.begin(9600);
@@ -33,11 +42,32 @@ void setup() {
   printSplash();
 
   delay(3000);
+  tft.fillScreen(ILI9341_BLACK);
+
+  startTime = millis();
 }
 
 void loop() {
-  //printReading(readTemp());
-  delay(5000);
+  unsigned long currentMillis = millis();
+
+  if((currentMillis % 1000L) == 0) {
+    elapsedTime = currentMillis - startTime;
+
+    displayTime(elapsedTime);
+  }
+
+  if(currentMillis - previousTempMillis > tempInterval) {
+    previousTempMillis = currentMillis;
+
+    float temp = readTemp();
+
+    // Don't update the display if the temp hasn't changed
+    if (temp != previousTemp) {
+      previousTemp = temp;
+
+      printReading(temp);
+    }
+  }
 }
 
 void printSplash() {
@@ -56,30 +86,66 @@ void clearScreen() {
 }
 
 void printHeadingTextLine(String text) {
-  tft.setTextSize(4);
+  tft.setTextSize(headingTextSize);
   tft.println(text);
 }
 
 void printSubheadingLine(String text) {
-  tft.setTextSize(2);
+  tft.setTextSize(subheadTextSize);
   tft.println(text);
 }
 
 float readTemp() {
-  //float temperature = probe.readTempF();
+  float temperature = probe.readTempF();
 
-  //if (!isnan(temperature)) {
-  //  return temperature;
-  //} else {
-  //  Serial.println("Could not read temp");
-  //  return 0;
-  //}
+  if (!isnan(temperature)) {
+    postTemp(temperature);
+
+    return temperature;
+  } else {
+    Serial.println("Could not read temp");
+    return 0;
+  }
+}
+
+void postTemp(float temp) {
+  String payload = "{ \"a\":" + String(temp, 2) + " }";
+  Particle.publish("BatchTemp", payload);
 }
 
 void printReading(float reading) {
-  tft.fillScreen(ILI9341_BLACK);
-  tft.setCursor(0, 160);
-  tft.setTextColor(ILI9341_WHITE);
-  tft.setTextSize(5);
+  tft.fillRect(0, 60, 240, tempTextSize * pixelMultiplier, ILI9341_BLACK);
+  tft.setCursor(0, 60);
+  tft.setTextSize(tempTextSize);
   tft.println(reading);
+}
+
+void displayTime(float elapsedTime) {
+  String timeString = calcTimeToDisplay(elapsedTime); //HERE
+
+  tft.fillRect(0, 140, 240, elapsedTimeSize * pixelMultiplier, ILI9341_BLACK);
+  tft.setCursor(0, 140);
+  tft.setTextSize(elapsedTimeSize);
+  tft.println(timeString);
+}
+
+String calcTimeToDisplay(float elapsedTime) {
+  String padZero = "0";
+  String timeString;
+
+  int timeInSec = (int)(elapsedTime / 1000L);
+  int timeInMin = timeInSec / 60;
+  int timeRemainder = timeInSec % 60;
+
+  if (timeInMin < 10) {
+    timeString = padZero;
+  }
+  timeString += String(timeInMin) + ":";
+
+  if (timeRemainder < 10) {
+    timeString += padZero;
+  }
+  timeString += timeRemainder;
+
+  return timeString;
 }
